8e8bedd6a2c5e2ba1ea2129cf498aa1e16e45beb
dxmari <dxmari007@gmail.com>
2021-03-13 14:24:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const { exec } = require('child_process');
+const express = require('express');
+const { json, urlencoded, raw, text} = require('body-parser');
+
+const app = express();
+
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(raw({type : 'text/html'}));
+
+const SUPPORTED_EXTENTIONS = ['txt', 'html'];
+
+const toPDF = (file, ext) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const source = path.resolve(__dirname, `${file.name}${ext}`);
+      const destination = path.resolve(__dirname, `${file.name}.pdf`);
+      fs.writeFileSync(source, file.content);
+      await execCMD(`html-pdf ${source} ${destination}`);
+      fs.unlinkSync(source);
+      console.log('save');
+      resolve();
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+const execCMD = (cmd) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err && err.code !== 0) {
+        reject(stderr);
+      } else {
+        resolve(stdout || stderr);
+      }
+    })
+  })
+}
+
+const findExt = (ext = 'html') => {
+  return `.${SUPPORTED_EXTENTIONS.find(e => e === ext) || 'html'}`;
+}
+
+app.post('/to-pdf', async (req, res) => {
+  const errorMsg = {};
+
+  if (!req.query.name) {
+    errorMsg.name = "File name is required"
+  }
+  if (Object.keys(req.body).length === 0) {
+    errorMsg.content = "Raw Content is required"
+  }
+  if (Object.keys(errorMsg).length > 0) return res.status(400).json(errorMsg);
+
+  const file = {
+    name: req.query.name,
+    content: req.body
+  }
+  toPDF(file, findExt(req.query.type)).then((pdfBuffer) => {
+    if (!req.query.download) {
+      res.sendFile(path.resolve(__dirname, file.name + '.pdf'));
+    } else {
+      res.download(path.resolve(__dirname, file.name + '.pdf'));
+    }
+  }).catch(error => {
+    console.log(error)
+    res.status(400).json({
+      error: error
+    })
+  })
+})
+
+
+app.get('*', async (req, res) => {
+  res.sendFile(path.resolve(__dirname, req.path.replace('/', '')), (err) =>{
+    if(!err){
+      fs.unlinkSync(path.resolve(__dirname, req.path.replace('/', '')));
+    }else{
+      res.status(400).json({
+        messsage : 'No file found'
+      });
+    }
+  });
+})
+
+const server = http.createServer(app);
+server.listen(9030, () => {
+  console.log('9030 is the magic port');
+})
